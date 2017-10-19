7721845ba8fdc92956dbb04787ed8bf0219205ca
sadjorlolo <sarah.adjorlolo@libertymutual.com>
2017-10-19 15:27:34 -0400

    Add user foreign key relationship to reviews
    - Generate a new migration AddUserToReviews
    - add belongs_to relationship to review
    - add has_many relationship to user
    - update review serializer to include :user

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,3 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :tea_name, :water_temp, :steep_time, :rating, :notes
+  attributes :id, :tea_name, :water_temp, :steep_time, :rating, :notes, :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019171619) do
+ActiveRecord::Schema.define(version: 20171019192133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20171019171619) do
     t.string "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +46,5 @@ ActiveRecord::Schema.define(version: 20171019171619) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "reviews", "users"
 end
