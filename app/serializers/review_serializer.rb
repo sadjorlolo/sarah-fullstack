35b24d5872480879f6400bea4a464a549dd33e49
sadjorlolo <sarah.adjorlolo@libertymutual.com>
2017-10-19 15:44:41 -0400

    Fix review serializer to not show all user info
    I had :users on the same line as attributes, but when I went to
    localhost.../reviews, all of the user's information was displaying.
    
    I switched :users to a new line with "belongs_to" and problem was
    resolved. Now only displaying serializer user data (id and email).

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,4 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :tea_name, :water_temp, :steep_time, :rating, :notes, :user
+  attributes :id, :tea_name, :water_temp, :steep_time, :rating, :notes
+  belongs_to :user
 end
